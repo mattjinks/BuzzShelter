720cbf185295324de511f4217a8d46d5e49056eb
Matthew Jinks <mattjinks@gmail.com>
2018-04-02 20:04:27 -0400

    Added Admin & HomelessPerson

---
+++ app/src/main/java/com/example/hp/buzzshelter/Admin.java
@@ -0,0 +1,13 @@
+package com.example.hp.buzzshelter;
+
+/**
+ * Created by HP on 3/20/2018.
+ */
+
+public class Admin extends User {
+
+    public Admin(String name, String loginEmail, String password) {
+        super(name, loginEmail, password);
+    }
+
+}
